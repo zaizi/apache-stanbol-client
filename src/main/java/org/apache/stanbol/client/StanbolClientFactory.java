15003c54d4a30277bb1ceb522de3c8ed4646e80e
Todd Shore <errantlinguist+github@gmail.com>
2015-04-03 02:04:37 +0200

    added toString() etc.

--- src/main/java/org/apache/stanbol/client/StanbolClientFactory.java
+++ src/main/java/org/apache/stanbol/client/StanbolClientFactory.java
@@ -37,7 +37,7 @@ public class StanbolClientFactory
      * @return Enhancer service
      */
 	
-	private UriBuilder builder;
+	private final UriBuilder builder;
 	
 	public StanbolClientFactory(String endpoint){
 		builder = UriBuilder.fromUri(endpoint);
@@ -64,4 +64,52 @@ public class StanbolClientFactory
     public final Sparql createSparqlClient(){
     	return new SparqlImpl(builder);
     }
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		StanbolClientFactory other = (StanbolClientFactory) obj;
+		if (builder == null) {
+			if (other.builder != null) {
+				return false;
+			}
+		} else if (!builder.equals(other.builder)) {
+			return false;
+		}
+		return true;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((builder == null) ? 0 : builder.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		StringBuilder builder2 = new StringBuilder();
+		builder2.append("StanbolClientFactory [builder=");
+		builder2.append(builder);
+		builder2.append("]");
+		return builder2.toString();
+	}
 }
